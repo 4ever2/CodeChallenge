c5a7ba2e4ef288a2d7928147db4204cd6cdd9eb0
Eske <eskehoy@gmail.com>
2019-04-14 23:24:16 +0200

    Finished SensorService.

--- CodeChallengeV2/Services/SensorService.cs
+++ CodeChallengeV2/Services/SensorService.cs
@@ -17,7 +17,62 @@ namespace CodeChallengeV2.Services
                 Time = payload.Time
             };
 
+            string data = payload.Data;
+            int port = payload.FPort;
+            var startIndex = 0;
+            
+            while(startIndex < data.Length) {
+                switch(port) {
+                    case 40:
+                        var tempInternal = getData(data.Substring(startIndex, 4)) / 100.0;
+                        startIndex += 4;
+
+                        res.TempInternal = tempInternal;
+                        break;
+                    case 41:
+                        var red = getData(data.Substring(startIndex, 4)) / 100.0;
+                        startIndex += 4;
+
+                        res.TempRed = red;
+                        break;
+                    case 42:
+                        var blue = getData(data.Substring(startIndex, 4)) / 100.0;
+                        startIndex += 4;
+
+                        res.TempBlue = blue;
+                        break;
+                    case 43:
+                        var temp = getData(data.Substring(startIndex, 4)) / 100.0;
+                        var humidity = getData(data.Substring(startIndex+4, 2)) / 2.0;
+                        startIndex += 6;
+
+                        res.TempHumidity = temp;
+                        res.Humidity = humidity;
+                        break;
+                    default:
+                        break;
+                }
+
+                if(startIndex < data.Length) {
+                    port = getData(data.Substring(startIndex, 2));
+                    startIndex += 2;
+                }
+            }
+
             return Task.FromResult(res);
         }
+
+        private short getData(string data)
+        {
+            char[] cData = data.ToCharArray();
+
+            string reversed = "";
+            for (int i = 0; i < cData.Length; i += 2) {
+                reversed = cData[i] + "" + cData[i+1] + "" + reversed;
+            }
+            
+            return Convert.ToInt16(reversed, 16);
+        }
+
     }
 }
