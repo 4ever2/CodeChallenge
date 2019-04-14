0c2c28532be4a290c561e08ad9a0ce741e3479ad
Eske <eskehoy@gmail.com>
2019-04-15 00:10:30 +0200

    Improved code for NetworkSumulatorService.

--- CodeChallengeV2/Services/NetworkSimulatorService.cs
+++ CodeChallengeV2/Services/NetworkSimulatorService.cs
@@ -16,18 +16,30 @@ namespace CodeChallengeV2.Services
         public Task<List<Node>> FindCritialGateways(NetworkGraph graph)
         {
             var network = graph.Graphs[0];
-            //var failures = new List<Node>();
+            var gateways = network.Nodes.Where(node => node.Type.Equals("Gateway"));
+            var devices = network.Nodes.Where(node => node.Type.Equals("Device"));
 
-            var disconnected = network.Nodes.Where(node => node.Type == "Device" && getEdges(network, node.Id).Count() == 1);
-            var failures = disconnected.Select(node => getEdges(network, node.Id).FirstOrDefault().Target);
-            var failureNodes = failures.Select(id => network.Nodes.FirstOrDefault(node => node.Id == id));
+            var disconnected = devices.Where(node => getEdges(network, node.Id).Count() == 1);
+            var failures = disconnected.Select(node => getEdges(network, node.Id).First().Target).ToList<string>();
+            //var failureNodes = failures.Select(id => network.Nodes.FirstOrDefault(node => node.Id == id));
 
-            return Task.FromResult(failureNodes.ToList<Node>());
+            var failureNodes = new List<Node>();
+
+            foreach (var s in failures)
+            {
+                var gateway = gateways.FirstOrDefault(node => node.Id.Equals(s));
+
+                if (gateway != null) {
+                    failureNodes.Add(gateway);
+                }
+            }
+
+            return Task.FromResult(failureNodes);
         }
 
         private List<Edge> getEdges(Graph graph, string id)
         {
-            return graph.Edges.Where(edge => edge.Source == id).ToList<Edge>();
+            return graph.Edges.Where(edge => edge.Source.Equals(id)).ToList<Edge>();
         }
     }
 }
