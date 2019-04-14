b0ab2d5030f90b80155147be3e394f6935bbf583
Eske <eskehoy@gmail.com>
2019-04-14 21:58:47 +0200

    Finished NetworkSimulatorService.

--- CodeChallengeV2/Services/NetworkSimulatorService.cs
+++ CodeChallengeV2/Services/NetworkSimulatorService.cs
@@ -15,7 +15,19 @@ namespace CodeChallengeV2.Services
         /// <returns></returns>
         public Task<List<Node>> FindCritialGateways(NetworkGraph graph)
         {
-            return Task.FromResult(Enumerable.Empty<Node>().ToList());
+            var network = graph.Graphs[0];
+            //var failures = new List<Node>();
+
+            var disconnected = network.Nodes.Where(node => node.Type == "Device" && getEdges(network, node.Id).Count() == 1);
+            var failures = disconnected.Select(node => getEdges(network, node.Id).FirstOrDefault().Target);
+            var failureNodes = failures.Select(id => network.Nodes.FirstOrDefault(node => node.Id == id));
+
+            return Task.FromResult(failureNodes.ToList<Node>());
+        }
+
+        private List<Edge> getEdges(Graph graph, string id)
+        {
+            return graph.Edges.Where(edge => edge.Source == id).ToList<Edge>();
         }
     }
 }
