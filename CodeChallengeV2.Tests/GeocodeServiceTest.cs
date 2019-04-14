01ee8335d39b4baa1e8e01e95eb920a28dc96d3d
Eske <eskehoy@gmail.com>
2019-04-14 21:27:15 +0200

    More reverse geocode tests.

--- CodeChallengeV2.Tests/GeocodeServiceTest.cs
+++ CodeChallengeV2.Tests/GeocodeServiceTest.cs
@@ -28,5 +28,56 @@ namespace CodeChallengeV2.Tests
 
             Assert.Equal("DK", res);
         }
+
+        [Theory]
+        [InlineData("10.231946", "56.202039")]
+        public async Task GeocodeTest2(string Long, string Lat)
+        {
+            var obj = new GeocodePayload()
+            {
+                DevEUI = "Test",
+                Time = DateTime.UtcNow,
+                Long = Long,
+                Lat = Lat
+            };
+
+            var res = await new GeocodeService().FindCountryCode(obj);
+
+            Assert.Equal("DK", res);
+        }
+
+        [Theory]
+        [InlineData("2.990787", "48.163032")]
+        public async Task GeocodeTest3(string Long, string Lat)
+        {
+            var obj = new GeocodePayload()
+            {
+                DevEUI = "Test",
+                Time = DateTime.UtcNow,
+                Long = Long,
+                Lat = Lat
+            };
+
+            var res = await new GeocodeService().FindCountryCode(obj);
+
+            Assert.Equal("FR", res);
+        }
+
+        [Theory]
+        [InlineData("-78.685603", "-1.088384")]
+        public async Task GeocodeTest4(string Long, string Lat)
+        {
+            var obj = new GeocodePayload()
+            {
+                DevEUI = "Test",
+                Time = DateTime.UtcNow,
+                Long = Long,
+                Lat = Lat
+            };
+
+            var res = await new GeocodeService().FindCountryCode(obj);
+
+            Assert.Equal("EC", res);
+        }
     }
 }
